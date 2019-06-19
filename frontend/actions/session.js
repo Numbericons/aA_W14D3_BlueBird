ef0339fc1adf3d292adaade69564eeedd12ea26d
ZachO <zachjoliver@gmail.com>
2019-06-19 09:04:53 -0700

    Complete video 6 material

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -10,5 +10,18 @@ export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 const receiveCurrentUser = user => ({
     type: RECEIVE_CURRENT_USER,
     user,
-})
+});
 
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER,
+});
+
+export const createNewUser = formUser => dispatch => postUser(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+// postUser -> ajax request, returns promise
+
+export const login = formUser => dispatch => postSession(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => deleteSession()
+    .then(() => dispatch(logoutCurrentUser()));
